de4ef2c38c2321d37428c8da5f711b8bbc2f54b6
Pranik Nikose <72003857+PranikNikose@users.noreply.github.com>
2026-01-22 23:54:34 +0530

    Update Launchgame.java

--- Assignment2/Launchgame.java
+++ Assignment2/Launchgame.java
@@ -5,79 +5,91 @@ import java.util.Scanner;
 class Guesser {
 	int guessNum;
 
-	public int guessNumber() {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Guesser kindly guess the number");
-		guessNum = scan.nextInt();
+	public int guessNumber(Scanner scan) {
+		do {
+			System.out.println("Guesser kindly guess the number between 1 to 9");
+			guessNum = scan.nextInt();
+		} while (guessNum < 1 || guessNum > 9);
 		return guessNum;
 	}
 }
 
 class Player {
-	int pguessNum;
+	int guessNum;
 
-	public int guessNumber() {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Player kindly guess the number");
-		pguessNum = scan.nextInt();
-		return pguessNum;
+	public int guessNumber(Scanner scan, int playerNo) {
+		do {
+			System.out.println("Player " + playerNo + " kindly guess the number between 1 to 9");
+			guessNum = scan.nextInt();
+		} while (guessNum < 1 || guessNum > 9);
+		return guessNum;
 	}
 }
 
 class Umpire {
 
 	int numFromGuesser;
-	int numFromPlayer1;
-	int numFromPlayer2;
-	int numFromPlayer3;
+	int[] playerGuesses;
 
-	public void collectNumFromGuesser() {
+	public void collectNumFromGuesser(Scanner scan) {
 		Guesser g = new Guesser();
-		numFromGuesser = g.guessNumber();
+		numFromGuesser = g.guessNumber(scan);
 	}
 
-	public void collectNumFromPlayer() {
-		Player p1 = new Player();
-		Player p2 = new Player();
-		Player p3 = new Player();
-		numFromPlayer1 = p1.guessNumber();
-		numFromPlayer2 = p2.guessNumber();
-		numFromPlayer3 = p3.guessNumber();
-	}
+	public void collectNumFromPlayers(Scanner scan, int numberOfPlayers) {
+		Player p = new Player();
+		playerGuesses = new int[numberOfPlayers];
 
-	void compare() {
-		if (numFromGuesser == numFromPlayer1) {
-			if (numFromGuesser == numFromPlayer2 && numFromGuesser == numFromPlayer3) {
-				System.out.println("Game tied all three players guessed correctly");
-			} else if (numFromGuesser == numFromPlayer2) {
-				System.out.println("Player 1 and Player2 won the game");
-			} else if (numFromGuesser == numFromPlayer3) {
-				System.out.println("Player 1 and Player3 won ");
-			} else {
-				System.out.println("Player 1 won the game");
-			}
-		} else if (numFromGuesser == numFromPlayer2) {
-			if (numFromGuesser == numFromPlayer3) {
-				System.out.println("Player 2 and Player3 won the game");
-			} else {
-				System.out.println("Player 2 won the game");
-			}
-		} else if (numFromGuesser == numFromPlayer3) {
-			System.out.println("Player 3 won the game");
-		} else {
-			System.out.println("Game lost! try again");
+		for (int i = 0; i < numberOfPlayers; i++) {
+			playerGuesses[i] = p.guessNumber(scan, i + 1);
 		}
 	}
 
+	void compare() {
+		int winnerCount = 0;
+
+		for (int guess : playerGuesses) {
+			if (guess == numFromGuesser) {
+				winnerCount++;
+			}
+		}
+
+		if (winnerCount == 0) {
+			System.out.println("Game lost! Try again");
+		} else if (winnerCount == playerGuesses.length) {
+			System.out.println("Game tied. All players guessed correctly");
+		} else {
+			System.out.print("Winner(s): ");
+			for (int i = 0; i < playerGuesses.length; i++) {
+				if (playerGuesses[i] == numFromGuesser) {
+					System.out.print("Player " + (i + 1) + " ");
+				}
+			}
+			System.out.println("won the game");
+		}
+	}
 }
 
 public class Launchgame {
 	public static void main(String[] args) {
-		Umpire u = new Umpire();
-		u.collectNumFromGuesser();
-		u.collectNumFromPlayer();
-		u.compare();
 
+		Scanner scan = new Scanner(System.in);
+		char choice;
+
+		do {
+			System.out.println("Enter number of players:");
+			int numberOfPlayers = scan.nextInt();
+
+			Umpire u = new Umpire();
+			u.collectNumFromGuesser(scan);
+			u.collectNumFromPlayers(scan, numberOfPlayers);
+			u.compare();
+
+			System.out.println("Do you want to play again? (y/n)");
+			choice = scan.next().toLowerCase().charAt(0);
+
+		} while (choice == 'y');
+
+		scan.close();
 	}
-
 }
